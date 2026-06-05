9fb1608cffd111fc750d1d3150963c18e648d15e
aldenisa <d3157190@gmail.com>
2026-06-05 12:13:09 +0300

    moved fakes

---
+++ UBB_SE_2026_Jobs.Tests/Helpers/CompanyBuilder.cs
@@ -0,0 +1,45 @@
+using UBB_SE_2026_Jobs.Library.Domain;
+
+namespace UBB_SE_2026_Jobs.Tests.Helpers;
+
+public class CompanyBuilder
+{
+    private int companyId = 1;
+    private string companyName = "Acme Corp";
+    private string logoText = "ACME";
+    private string email = "hr@acme.test";
+    private string phone = "+40000000000";
+
+    public CompanyBuilder WithId(int id)
+    {
+        companyId = id;
+        return this;
+    }
+
+    public CompanyBuilder WithName(string value)
+    {
+        companyName = value;
+        return this;
+    }
+
+    public CompanyBuilder WithEmail(string value)
+    {
+        email = value;
+        return this;
+    }
+
+    public CompanyBuilder WithPhone(string value)
+    {
+        phone = value;
+        return this;
+    }
+
+    public Company Build() => new()
+    {
+        CompanyId = companyId,
+        CompanyName = companyName,
+        LogoText = logoText,
+        Email = email,
+        Phone = phone,
+    };
+}
