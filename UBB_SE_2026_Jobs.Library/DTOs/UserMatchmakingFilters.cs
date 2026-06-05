5c84245e55cc37310c6ec8d59eb90f2ada66a9c1
Eduard-Alaci <eduard.alaci@stud.ubbcluj.ro>
2026-06-05 18:42:22 +0300

    FiX Filter

--- UBB_SE_2026_Jobs.Library/DTOs/UserMatchmakingFilters.cs
+++ UBB_SE_2026_Jobs.Library/DTOs/UserMatchmakingFilters.cs
@@ -4,6 +4,7 @@ public sealed class UserMatchmakingFilters
 {
     public HashSet<string> EmploymentTypes { get; init; } = new(StringComparer.OrdinalIgnoreCase);
     public HashSet<string> ExperienceLevels { get; init; } = new(StringComparer.OrdinalIgnoreCase);
+    public HashSet<string> WorkModes { get; init; } = new(StringComparer.OrdinalIgnoreCase);
     public string LocationSubstring { get; set; } = string.Empty;
     public HashSet<int> SkillIds { get; init; } = new();
 
