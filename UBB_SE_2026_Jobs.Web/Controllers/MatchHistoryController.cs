d48aadc89d5e57b188d68ae40ffc2126706a9466
Violeta-16 <ungureanuvioletamihaela@gmail.com>
2026-06-06 12:08:24 +0300

    added match hitory to the web

---
+++ UBB_SE_2026_Jobs.Web/Controllers/MatchHistoryController.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UBB_SE_2026_Jobs.Library.Services.Matches;
+
+namespace UBB_SE_2026_Jobs.Web.Controllers;
+
+[Authorize(Roles = "Candidate")]
+public class MatchHistoryController : Controller
+{
+    private readonly IMatchService matchService;
+
+    private int CurrentUserId =>
+        int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+    public MatchHistoryController(IMatchService matchService)
+    {
+        this.matchService = matchService;
+    }
+
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    {
+        var matches = await matchService.GetMatchesForUserAsync(CurrentUserId, cancellationToken);
+        var statistics = await matchService.GetMatchStatisticsAsync(CurrentUserId, cancellationToken);
+
+        ViewBag.Statistics = statistics;
+        return View(matches);
+    }
+}
